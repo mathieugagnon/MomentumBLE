1483835034594d87ba763831781e72c253a1b826
Mathieu <math_gagnon11@live.ca>
2018-11-18 01:16:54 +0000

    Data test formatting. Execption handled

--- BLE.py
+++ BLE.py
@@ -9,23 +9,38 @@ import platform
 from bluepy.btle import Scanner, DefaultDelegate, Peripheral, BTLEException
 import bluepy
 
-# Service for confirmation                                                 67888895-69db-4168-8df4-9e12a10fa5b7
+# Service for confirmation                                                  67888895-69db-4168-8df4-9e12a10fa5b7
 #define COPY_DATA_SERVICE_UUID(uuid_struct)      COPY_UUID_128(uuid_struct,0x67,0x88,0x88,0x95, 0x69,0xdb, 0x41,0x68, 0x8d,0xf4, 0x9e,0x12,0xa1,0x0f,0xa5,0xb7)
 
-# Send to this char RDY                                                    713a155c-5ae2-4604-9ec4-078ad627926e
-#define COPY_READY_CHAR_UUID(uuid_struct)        COPY_UUID_128(uuid_struct,0x71,0x3a,0x15,0x5c, 0x5a,0xe2, 0x46,0x04, 0x9e,0xc4, 0x07,0x8a,0xd6,0x27,0x92,0x6e)
+
+# Send to this UUID     LENGTH DATA IN                                      713a155c-5ae2-4604-9ec4-078ad627926e
+#define COPY_DATAIN_CHAR_UUID(uuid_struct)        COPY_UUID_128(uuid_struct,0x71,0x3a,0x15,0x5c, 0x5a,0xe2, 0x46,0x04, 0x9e,0xc4, 0x07,0x8a,0xd6,0x27,0x92,0x6e)
+
+#                                                                           e7a87ad7-9ce0-40d0-896f-7fbc3d10d9ff
+#DATA IN (send to proto on this UUID) (VOIR FB)   UUID (0xe7,0xa8,0x7a,0xd7, 0x9c,0xe0, 0x40,0xd0, 0x89,0x6f, 0x7f,0xbc,0x3d,0x10,0xd9,0xff)    
+
 
 # Reception de donné                                                       04461336-088e-418e-b8aa-606bdcb8f300
 #define COPY_DATA_CHAR_UUID(uuid_struct)         COPY_UUID_128(uuid_struct,0x04,0x46,0x13,0x36, 0x08,0x8e, 0x41,0x8e, 0xb8,0xaa, 0x60,0x6b,0xdc,0xb8,0xf3,0x00)
 
 # WatchDog Service for Confirmation
 #define COPY_WATCH_DOG_SERVICE_UUID(uuid_struct) COPY_UUID_128(uuid_struct,0x1a,0x49,0xd9,0x22, 0xe3,0xa9, 0x4b,0x00, 0x92,0x53, 0xc4,0xc7,0x2a,0x1b,0xcb,0x5d)
-
-# WatchDog Characteristic
-#define COPY_WATCH_DOG_CHAR_UUID(uuid_struct)    COPY_UUID_128(uuid_struct,0x78,0x95,0xce,0xcb, 0xa9,0x23, 0x4c,0xe4, 0xbc,0x58, 0x27,0xe8,0xce,0x6e,0x00,0xea)
+   
 
 
 max_size = 20
+index = 0
+__BleDataInBuffer__ = bytearray()
+
+def filldatabuffer(data):
+    sizeData = len(data)
+    if sizeData > 20:
+        print("Data buffer too long")   
+    else:
+        __BleDataInBuffer__.extend(data)    
+        # print("{}".format(databuffer))
+        # print(self.Mydata)
+# Fill buffer with data and verify length
 
 
 # Using bluepy
@@ -43,37 +58,22 @@ class NotifyDelegate(DefaultDelegate):
         DefaultDelegate.__init__(self)
         self.hndl = hndl
 
-
-
-# Fill buffer with data and verify length
-    def filldatabuffer(self,data):
- #       self.databuffer = bytearray()
-        print("Buffer length is : {}".format(len(data)))
-        print("Buffer data : {}".format(data))
-        self.databuffer = data
-        if len(self.databuffer) > 20:
-            print("Data buffer too long")   
-        else:
-            print("Data buffer is < 20")
-
-
+    
     def handleNotification(self, cHandle, dataIN):
         if self.hndl == cHandle:           
 #            print("Handle : {}, data : {}".format(cHandle, data))
-            self.filldatabuffer(dataIN)
+            filldatabuffer(dataIN)
+        
         else:
             print("Wrong handle called.")
             print("self.hndl : {} != cHandle : {}".format(self.hndl, cHandle))
- 
+
 
 
 # Bluetooth classes
 class BleDevice:
- 
-
     def __init__(self):
         self.scanner = Scanner().withDelegate(ScanDelegate())
-
 
 
     def Scan(self, time):
@@ -105,6 +105,7 @@ class BleDevice:
                 if adtype == 9:
                     if value == "Boogie":
                         boogie = dev
+                        print('OK : Boogie Device Found')
         return boogie
 
 
@@ -112,13 +113,13 @@ class BleDevice:
         # Function returns Connected to device when device is not there
            # print('EXIT IS : {}'.format(device))
             if device is None:
-                print("No device detected")
+                print('ERROR : No device detected')
             else:
                 try:
                     self.peripheral = Peripheral(device)
-                    print("Connected to device")
+                    print('OK : Connected to device')
                 except BTLEException as e:
-                    print('Connect to device failed with {} ', format(e))
+                    print('ERROR : Connection to device failed with exception : {} ', format(e))
 
 
 # Service UUID for Watchdog Only
@@ -126,15 +127,18 @@ class BleDevice:
         UUID_ack = False      
         try:      
             self.service = self.peripheral.getServiceByUUID(S_UUID)
-        except BTLEException as e:
-            print('Error in finding service UUID for WD with : {}',format(e))
-
-        if str(self.service.uuid) == S_UUID:
-            UUID_ack = True
-            print('Service WatchDog Verified')
-        else:
-            UUID_ack=False
-            print('Search WatchDig Service by UUID error')
+        except Exception as e:
+            print('ERROR : Finding service UUID for WD failed with exception {}',format(e))
+        
+        try:
+            if str(self.service.uuid) == S_UUID:
+                UUID_ack = True
+                print('OK : Service WatchDog Verified')
+            else:
+                UUID_ack=False
+                print('ERROR : WatchDog Service UUID not found')
+        except Exception as e:
+            print('ERROR : Finding WD Service UUID failed with exception : {} ', format(e))
 
 
 # Characteristic UUID for Watchdog Only
@@ -144,43 +148,25 @@ class BleDevice:
         try:
             Service_characteristics = self.service.getCharacteristics()
         except Exception as e:
-            print('Error in finding Characteristic UUID for WD with : {}',format(e))
+            print('ERROR : Finding Characteristic UUID failed with exception : {}'.format(e))
         
         for characteristic in Service_characteristics:
             if str(characteristic.uuid) == C_UUID:
                 CHARACT_ack = True
                 BoogieWD_Characteristic = characteristic
-                print('Characteristic WatchDog Verified')
+                print('OK : Characteristic WatchDog Verified')
             else:
-                print('Characteristic WatchDog error')
-        
+                print('ERROR : Characteristic WatchDog not found')
         return BoogieWD_Characteristic       
 
 
-# Read Characteristics for Watchdog Only
+# Read Characteristics for Watchdog Only (Unused fonction)
     def ReadCharacteristic_WD(self, Boogie_Characteristic_WD):
         try:
-            Info = Boogie_Characteristic_WD.read()
-            print('My WD Characteristic value is : {}'.format(Info))
+            WD_Characteristic_Data = Boogie_Characteristic_WD.read()
+            print('My WD Characteristic value is : {}'.format(WD_Characteristic_Data))
         except Exception as e:
             print('Error in reading Watchdog Characteristic with : {}'.format(e))
-
-
-#This function send a RDY to receive data. (VERIFY THIS FONCTION LAST) --------------- TO DELETE --------------
-#    def ReadyToReceiveData(self, RdyServiceUUID, RdyCharUUID):
-#        RdyCharacteristic = None
-#                
-#        RdyService = self.peripheral.getServiceByUUID(RdyServiceUUID)
-#        RdyCharacteristics = RdyService.getCharacteristics()       
-#        for Characteristic in RdyCharacteristics:
-#            if Characteristic.uuid == RdyCharUUID:
-#                RdyCharacteristic = Characteristic
-#                print('Characteristic Data RDY is : {}'.format(RdyCharacteristic))
-#    # Write random data to characteristic to verify if communication is valid. 
-#                code = '1'
-#                bytes1 = bytes(code, 'utf-8')
-#                RdyHandle = RdyCharacteristic.getHandle()
-#                RdyCharacteristic.write(bytes1)
 
 
 # Get handle from service and data characteristic
@@ -191,43 +177,44 @@ class BleDevice:
         try:
             self.Service = self.peripheral.getServiceByUUID(ServiceUUID)
         except Exception as e:
-            print('Error in Subscribe function, getServiceByUUID : {} '.format(e))
+            print('ERROR : Subscribtion function failed to get service by UUID with exception : {}'.format(e))
 
         try:
             self.Characteristics = self.Service.getCharacteristics()
         except Exception as e:
-            print('Error in Subscribe function, getServiceByUUID : {} '.format(e))
+            print('ERROR : Subscribtion function failed to get characteristic by UUID with exception : {}'.format(e))
 
         for Characteristic in self.Characteristics:
 #            print("Printing Char UUID : {} ".format(Characteristic))
             if Characteristic.uuid == DataCharacteristicUUID:
                 self.DataHandle = Characteristic.getHandle()
                 self.DataHDL = self.DataHandle + 1
-#                print('Data Handle # is : {}'.format(self.DataHDL))
                 DataUUID_OK = True
+                print('OK : Data characteristic UUID found. Handle is : {}'.format(self.DataHDL))
                 break;
 
         if DataUUID_OK == False:
-            print('Characteristic UUID for Data not found')
+            print('ERROR : Data Characteristic UUID not found')
 
 
     def InitializeDataTransfert(self, code):
         bytes1 = bytes(code, 'utf-8')
         try:
             self.peripheral.writeCharacteristic(self.DataHDL,bytes1)
-            print('Data transfert initialization complete to handle : {}'.format(self.DataHDL))
+            print('OK : Data transfert initialization Complete')
         except Exception as e:
-            print('Error in Data transfert initialization : {}'.format(e))
+            print('ERROR : Data transfert initialization failed with exception : {}'.format(e))
 
 
     def ReceivingData(self):
         try:
-            self.peripheral.setDelegate(NotifyDelegate(self.DataHandle))
-           # VERIFIER SI LE SET DELEGATE RETOURNE DE QUOI POUR APPELLER ADD BLE DEVICE DANS LA CLASSE NOTIFY ET QUE LES 2 CLASSE APPELE LES MEME FONCTIONS 
+            self.peripheral.setDelegate(NotifyDelegate(self.DataHandle)) 
+            
         except Exception as e:
-            print('Error in Receiving data : {}'.format(e))
+            print('ERROR : Setting delegate to receive data failed with exception : {}'.format(e))
         while True:
-            if  self.peripheral.waitForNotifications(5)== False:      
+            if  self.peripheral.waitForNotifications(5)== False:
+                print('OK : Data received : {}'.format(__BleDataInBuffer__))         
                 break
 #        writewithresponse()
 
@@ -243,8 +230,7 @@ class BleDevice:
                 except Exception as e:
                     print('TIMEOUT : No response received')
                 
-             
 
-
-
+ #   def SendData(self, message):
+        
 
--- test_ble.py
+++ test_ble.py
@@ -7,48 +7,65 @@ print("Python : " + platform.python_version())
 #print("Bluepy : " + bluepy.__version__)
 print("---------------")
 
+n = 1
 
 # Main Code -----------------------------------------------------------
-print("test 1")
+#print("Test",n, ": Creation d'un objet BLEdevice")
+#n=n+1
+
 ble = BleDevice()
+print("------------------------------------------------------------------")
 
-print("test 2")
+
+print("Test",n, ": Detection of Boogie companion")
+n=n+1
 ble.Scan(5.0)
-
-print("test 3")
 boogie = ble.FindBoogie()
+print("------------------------------------------------------------------")
 
-print("test 4")
+
+print("Test",n, ": Connection to Boogie ")
+n=n+1
 #Find device named boogie in device list
 ble.ConnectToDevice(boogie)
+print("------------------------------------------------------------------")
 
-print("test 5")
+
+print("Test",n,": ID Verification")
+n=n+1
 #Find Watchdog service from Device and validate the UUID
 S_UUID = "1a49d922-e3a9-4b00-9253-c4c72a1bcb5d"
 ble.GetServiceByUUID_WD(S_UUID)
-
-print("test 6")
 #Find Watchdog characteristic and validate the UUID
 C_UUID =  "7895cecb-a923-4ce4-bc58-27e8ce6e00ea"
-BoogieCharacteristic= ble.GetCharacteristicsByUUID_WD(C_UUID) 
+BoogieCharacteristic = ble.GetCharacteristicsByUUID_WD(C_UUID) 
+print("------------------------------------------------------------------")
 
-print("test 7")
+
+print("Test",n,": Subcription to Indication")
+n=n+1
 #Find Data Handle from data characteristic UUID. Prepare Handle+1 for writing
 DataCharUUID = '04461336-088e-418e-b8aa-606bdcb8f300'
 ServiceUUID = '67888895-69db-4168-8df4-9e12a10fa5b7'
 ble.SubcribeToIndication(ServiceUUID, DataCharUUID)
+print("------------------------------------------------------------------")
 
-print("test 8")
+
+print("Test",n,": Data Transfert Initialization")
+n=n+1
 #Initialize Data transfert by writing 2 in bytes to the data characteristic
 code = "\x02\x00"
 ble.InitializeDataTransfert(code)
+print("------------------------------------------------------------------")
 
 
-print("test 9")
+print("Test",n,": Receiving Data")
+n=n+1
 #Enter a while loop to receive data. exit the while loop when timeout(10sec) is reach.
 ble.ReceivingData()
+print("------------------------------------------------------------------")
 
 
-#Char_WithResponseUUID = ""
-#ble.writewithresponse(Char_WithResponseUUID)
+
+
 
